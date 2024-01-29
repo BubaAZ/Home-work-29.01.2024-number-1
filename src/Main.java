ef9d2b0d83ee51d75ae19206ef127915d6e31196
Alex Zakharov <zakharov61@yandex.ru>
2024-01-29 19:46:10 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         float tikerPrise = 25659.88F;
-                int milebonus = 20;
-                        int mile = (int)tikerPrise / milebonus;
+        int milebonus = 20;
+        int mile = (int) tikerPrise / milebonus;
         System.out.println("Вам придет " + mile + " миль за купленный билет");
 
     }
